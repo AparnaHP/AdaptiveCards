435425e9bb778e45f90906d004362447ef66b5f0
AparnaHP <aparna@ascentic.se>
2020-10-08 18:11:18 +0530

    keyboardShouldPersistTaps="always"

--- source/community/reactnative/src/adaptive-card.js
+++ source/community/reactnative/src/adaptive-card.js
@@ -184,7 +184,7 @@ export default class AdaptiveCard extends React.Component {
 					: undefined}>
 					<ContainerWrapper style={containerStyles} json={this.state.cardModel}>
 						<ScrollView
-							keyboardShouldPersistTaps="handled"
+							keyboardShouldPersistTaps="always"
 							showsHorizontalScrollIndicator={true}
 							showsVerticalScrollIndicator={true}
 							alwaysBounceVertical={false}
