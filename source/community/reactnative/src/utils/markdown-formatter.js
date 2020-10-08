1f5f52f32d87471a7024a68b7c3ad4eaceea7224
AparnaHP <aparna@ascentic.se>
2020-10-08 19:37:49 +0530

    Comment the slice new line function

--- source/community/reactnative/src/utils/markdown-formatter.js
+++ source/community/reactnative/src/utils/markdown-formatter.js
@@ -169,11 +169,11 @@ export default class MarkdownFormatter extends React.PureComponent {
 		}
 
 		// slice the new line character and update the text
-		matched.forEach(value => {
+		/*matched.forEach(value => {
 			if (this.text[value.index] === '\n' || this.text[value.index] === '\r') {
-				// this.text = this.text.slice(0, value.index) + " " + this.text.slice(value.index + 1);
+				this.text = this.text.slice(0, value.index) + " " + this.text.slice(value.index + 1);
 			}
-		});
+		});*/
 	}
 
 	/**
